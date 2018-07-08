22a04bb7e8c07ed86e41f54e6e39dc618db8e203
Matthew Noseworthy <mjn327@mun.ca>
2018-07-08 16:37:09 -0230

    Restructured source for easier test workflow

--- src/pcb_region_from_video.py
+++ src/pcb_region_from_video.py
@@ -13,165 +13,248 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+class pcb_region_detection():
+    def __init__(self):
+        ###################################################################
+        #   Section: Define attributes
+        ###################################################################
+        # Capture object from opencv
+        self.cap = None
 
-def hist_equalize(img):
-    """
-        Equalizes a color image, returning the result in color
-    """
-    yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
-    yuv[:,:,0] = cv2.equalizeHist(yuv[:,:,0])
-    return cv2.cvtColor(yuv, cv2.COLOR_YUV2BGR)
+        # Most recently captured frame
+        self.frame = None
 
-def find_overlay_region(img, display = False):
-    """
-        Locates the PCB in the given image frame.
-        @param img (cv2.image) - Pointer to an openCV2 image object, returned from imread().
-        @param display (bool) - If set to true, the image will be displayed as the code runs, for debug usage.
-        @returns [(cv2.contour), [(int, int)] - Returns a tuple containing 1) the contour object returned by opencv & 2)
-            the x,y co-ordinates of the centre point of the contour
-    """
-    # Make a copy of image if we're going to display
-    if display:
-        original = img.copy()
-    #img[:,:,0] = 0
-    #img[:, :, 2] = 0
-    # Calc image dimensions
-    height, width, channels = img.shape
-    area = height*width
+        # Processing buffer
+        self.buffer = {
+            "Input" : None,
+            "equalized" : None,
+            "blurred" : None,
+            "thresholded" : None,
+            "morphed" : None,
+            "edged" : None,
+            "contours" : None,
+            "Output" : None
+        }
 
-    ## (1) Convert to gray, and threshold green channel, remove others
-    #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # Equalize histogram
-    equalized = hist_equalize(img)
-    hsv = cv2.cvtColor(equalized, cv2.COLOR_BGR2HSV)
-    # Hue (0-180) saturation brightness
-    #lower = np.array([35,100,50])
-    #upper = np.array([75, 190,175])
+        # Processing error flag
+        self.failure = False
 
-    # Blur
-    #blur = cv2.GaussianBlur(hsv, (5,5), 0)
-    blur = cv2.medianBlur(hsv,5)
-    #Filter w/ green threshold
-    lower = np.array([45,100,100])
-    upper = np.array([82, 180,180])
-    green_mask = cv2.inRange(blur, lower, upper)
-    green_thresh = cv2.bitwise_and(img, img, mask=green_mask )
+        # Flag to determine weather or not to display results
+        # False = Just display output frame
+        # True = Display all 
+        # None = no display
+        self.display = None
 
+        # List of function pointers which the input frame is to be passed through
+        self.function_pipe = [
+            hist_equalize,
+            bgr_to_hsv,
+            blur_before_thresh,
+            hsv_green_thresholding,
+            morphology_operation,
+            canny_edge_detection,
+            contour_filter
+        ]
 
-    #morphed = threshed
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11,11))
-    morphed = cv2.morphologyEx(green_thresh, cv2.MORPH_CLOSE, kernel)
-    # 
-    edged_frame = cv2.Canny(green_thresh,100,110)
-
-    # Gaussian blur then Otsu's thresholding
+        ###################################################################
+        #   Section: Define init control flow
+        ###################################################################
+        self.videoCapStart()
     
-    #ret, threshed = cv2.threshold(blur, 50, 200, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    def clearBuffers():
+        """
+            Emptys buffer data
+        """
+        for key, value in self.buffer:
+            self.buffer[key] = None
 
-
-
-    ## (2) Morph-op to remove noise
-
-
-    ## (3) Find the max-area contour, which should be the PCB
-    _, cnts, im = cv2.findContours(edged_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = sorted(cnts, key=cv2.contourArea)
-    index = len(cnts) - 1
-    cnt = False
-    print("Found {} contours".format(len(cnts)))
-    # Find rectangular contours
-    """
-    rect_cnts = []
-    while index >= 0:
-        perimeter = cv2.arcLength(cnts[index], True)
-        approx = cv2.approxPolyDP(cnts[index], 0.01 * perimeter, True)
-        if len(approx) == 4:
-            print("Found rectangular contour")
-            rect_cnts.append(cnts[index])
-        index -= 1
-
-    rect_cnts = sorted(cnts, key=cv2.contourArea)
-    index = len(rect_cnts) - 1
+    def hist_equalize(img):
+        """
+            Equalizes a color image, returning the result in color
+        """
+        yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+        yuv[:,:,0] = cv2.equalizeHist(yuv[:,:,0])
+        self.buffer["equalized"] = cv2.cvtColor(yuv, cv2.COLOR_YUV2BGR)
+        return self.buffer["equalized"]
     
+    def bgr_to_hsv(img):
+        """
+            Converts a bgr array to hsv format
+        """
+        return cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    while index >= 0:
-        #print("Image area = {}, contour area = {}".format(area*0.90, cv2.contourArea( cnts[index] )))
-        if cv2.contourArea( rect_cnts[index] ) < (area*0.90):
-            cnt = rect_cnts[index]
-            break
-    """
-    x = 0
-    y = 0
-    if(len(cnts)):
-        cnt = cnts[-1]
+    def blur_before_thresh(img):
+        """
+            Blurs an image, to be used before inputing the return through
+            a thresholding algorithm
+        """
+        self.buffer["blurred"] = cv2.medianBlur(img, 5)
+        return self.buffer["blurred"]
 
-        #if isinstance(cnt, (int, float)) and cnt == False:
-        #    print("Error finding largest contour, setting to default")
-        #    cnt = rect_cnts[-1]
+    def hsv_green_thresholding(img):
+        """
+            Uses a HSV format image, and thresholds based on hue to remove all colors
+            but the range of greens we expect a PCB to be
+        """
+        lower = np.array([45,100,100])
+        upper = np.array([82, 180,180])
+        green_mask = cv2.inRange(img, lower, upper)
+        self.buffer["morphed"] = cv2.bitwise_and(img, img, mask=green_mask )
+        return self.buffer["morphed"]
 
-        # Calculate center of contour
-        moment = cv2.moments(cnt)
-        if(moment["m00"] != 0):
-            x = int( moment["m10"] / moment['m00'] )
-            y = int( moment['m01'] / moment['m00'] )
+    def morphology_operation(img):
+        """
+            Uses morphology to attempt to reduce salt and pepper noise
+        """
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11,11))
+        self.buffer["morphed"] = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+        return self.buffer["morphed"]
+    
+    def canny_edge_detection(img):
+        """
+            Performes canny edge detection on the image and returns the result
+        """
+        self.buffer["edged"] = cv2.Canny(img,100,110)
+        return self.buffer["edged"]
 
-        cv2.drawContours(img, [cnt], -1, (0, 0, 255), 1)
+    def contour_filter(img):
+        """
+            After all image processing operations have completed, we're left with an 
+            array of contours that we must filter to output our best guess at which
+            contour contains the PCB.
+        """
+        height, width, channels = img.shape
+        area = height*width
+        _, cnts, im = cv2.findContours(edged_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = sorted(cnts, key=cv2.contourArea)
+        index = len(cnts) - 1
+        cnt = False
+        print("Found {} contours".format(len(cnts)))
+        # Find rectangular contours
+        """
+        rect_cnts = []
+        while index >= 0:
+            perimeter = cv2.arcLength(cnts[index], True)
+            approx = cv2.approxPolyDP(cnts[index], 0.01 * perimeter, True)
+            if len(approx) == 4:
+                print("Found rectangular contour")
+                rect_cnts.append(cnts[index])
+            index -= 1
 
-    # Draw the contour onto the original image
-    if display:
+        rect_cnts = sorted(cnts, key=cv2.contourArea)
+        index = len(rect_cnts) - 1
         
-        #cv2.drawContours(img, rect_cnts, -1, (255, 0, 0), 3)
-        output = np.hstack((original, img ))
-        cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('image', 800,800)
-        cv2.imshow("Final output", output)
-        cv2.imshow("Thresholded Image", green_thresh)
-        cv2.imshow("Canny on Threshold", edged_frame)
-        cv2.imshow("Morphed edge detection frame", morphed)
-        #if cv2.waitKey(0) & 0xff == 27:
-        #    cv2.destroyAllWindows()
+
+        while index >= 0:
+            #print("Image area = {}, contour area = {}".format(area*0.90, cv2.contourArea( cnts[index] )))
+            if cv2.contourArea( rect_cnts[index] ) < (area*0.90):
+                cnt = rect_cnts[index]
+                break
+        """
+
+        if(len(cnts)):
+            return cnts[-1]
+        else:
+            return False
+
+
+    def find_overlay_region():
+        """
+            Locates the PCB in the given image frame.
+            @param img (cv2.image) - Pointer to an openCV2 image object, returned from imread().
+            @param display (bool) - If set to true, the image will be displayed as the code runs, for debug usage.
+            @returns [(cv2.contour), [(int, int)] - Returns a tuple containing 1) the contour object returned by opencv & 2)
+                the x,y co-ordinates of the centre point of the contour
+        """
+        # Check if we need to pull a new frame
+        if self.frame == None:
+            self.getFrame()
+
+        # Clear buffers
+        self.clearBuffers()
+
+        # Setup return buffer [Contour Matrix, [Center X, Center Y]] 
+        ret = [False, [False, False]]
+
+        # Make a copy of image if we're going to display
+        if display != None:
+            self.buffers["Input"] = self.frame
+
+        # Push frame through the function pipe, result should be a single contour
+        try:
+            result = None
+            for funct in self.function_pipe:
+                # First function requires input frame
+                if result = None:
+                    funct(self.frame)
+                else:
+                    funct(result)
+        except:
+            self.failure = True
+            pass
+        
+        if result and not self.failure:
+            # Calculate center of contour
+            moment = cv2.moments(result)
+            if(moment["m00"] != 0):
+                x = int( moment["m10"] / moment['m00'] )
+                y = int( moment['m01'] / moment['m00'] )
+
+            cv2.drawContours(self.frame, [result], -1, (0, 0, 255), 1)
+
+            # If true, output all intermediate images as well as Input/Output
+            if display == True: 
+                cv2.imshow("Input", self.buffer["Input"])
+                cv2.imshow("Output", self.buffer["Output"])
+                cv2.imshow("equalized". self.buffer["equalized"])
+                cv2.imshow("morphed". self.buffer["morphed"])
+                cv2.imshow("thresholded". self.buffer["thresholded"])
+                cv2.imshow("blurred". self.buffer["blurred"])
+                cv2.imshow("edged". self.buffer["edged"])
+            # If false output just Input/Output frames
+            elif display == False:
+                cv2.imshow("Input", self.buffer["Input"])
+                cv2.imshow("Output", self.buffer["Output"])
+   
+            # Return the contour & centre coordinates
+            ret = [cnt, [x,y] ]
+        else:
+            ret = [False, [False, False]]
+        
+        # Clear processed frame
+        self.frame = None
+
+        # Return values
+        return ret
+
+
+
+    def videoCapStart():
+        self.cap = cv2.VideoCapture(0)
+        return self.cap
+
+    def videoCapStop():
+        self.cap.release()
     
-    # Return the contour & centre coordinates
-    return [cnt, [x,y] ]
+    def getFrame():
+        self.frame = self.cap.read()
+        return self.frame
 
-
-def videoCapStart():
-    return cv2.VideoCapture(0)
-
-def videoCapStop(cap):
-    cap.release()
-
-def mainThread(cap):
-    greenLow = 100
-    greenHigh = 155
-
-    frameCount = 0
-    while(1):
-        ret, frame = cap.read()
-        find_overlay_region(frame, greenLow, greenHigh, True)
-        frameCount = frameCount + 1
-        if(frameCount % 25 == 0):
-            greenLow = greenLow + 1
-            greenHigh = greenHigh + 1
-            frameCount = 0
-        k= cv2.waitKey(5)
-        if k==27:
-            break
+    def mainThread():
+        self.display = True
+        while(1):
+            try:
+                self.getFrame()
+                self.get_overlay_region()
+            except:
+                self.videoCapStop()
+                break
+        return False
  
 
 
 if __name__ == "__main__":
-    # Load image
-    #path = "assets/test0.jpg"
-    #image = cv2.imread(path)
-    
-
-    # Print image size
-    #height, width = image.shape[:2]
-    #print("Input image is {} by {} ".format(height, width))
-
-    # Get biggest contour and mask
-    cap = videoCapStart()
-    mainThread(cap)
-    videoCapStop(cap)
+    # Create object and start main thread
+    pcb_det = pcb_region_detection()
+    pcb_det.mainThread()
 
