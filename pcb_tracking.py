f3a94b01f8c4e8c851e59c63f74c6375d3f4d29d
Matthew Noseworthy <mjn327@mun.ca>
2018-07-07 22:35:21 -0400

    Added python 3 support

--- pcb_tracking.py
+++ pcb_tracking.py
@@ -27,13 +27,13 @@ if __name__ == "__main__":
 
     # Exit if video not opened.
     if not video.isOpened():
-        print "Could not open video"
+        print("Could not open video")
         sys.exit()
 
     # Read first frame.
     ok, frame = video.read()
     if not ok:
-        print 'Cannot read video file'
+        print( 'Cannot read video file')
         sys.exit()
     
     # Define an initial bounding box
